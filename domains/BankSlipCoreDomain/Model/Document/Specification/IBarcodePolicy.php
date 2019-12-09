ad5df39f6f78f424af9cbef403e5d25388f15c45
Renato Costa <renatocostahome@gmail.com>
2019-12-09 14:35:21 -0300

    Add policies specification

--- domains/BankSlipCoreDomain/Model/Document/Specification/IBarcodePolicy.php
+++ domains/BankSlipCoreDomain/Model/Document/Specification/IBarcodePolicy.php
@@ -2,6 +2,10 @@
 
 namespace BankSlipCoreDomain\Model\Document\Specification;
 
+/**
+ * Interface it is for create a specification that is able to tell if a incoming data matches some criteria.
+ *
+ */
 interface IBarcodePolicy
 {
 
